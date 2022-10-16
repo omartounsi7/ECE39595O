9bc78e6a009ec39d2c131ffb4a1948b91e0db8e0
omartounsi7 <otounsi@purdue.edu>
2022-10-16 17:25:46 -0400

    some comments

--- ProjectCode/ProjectCode/main.cpp
+++ ProjectCode/ProjectCode/main.cpp
@@ -7,3 +7,13 @@ int main(int argc, char* argv[]) {
 
 
 }
+
+/* Omar: We need to do the following:
+1) Open up the file containing the instructions
+2) Read the instructions one by one.
+    An instruction is of form: opcode [operand [operand]].
+    We need to read the opcode first.
+3) Execute the instructions one by one.
+
+
+*/
