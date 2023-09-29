289f2678e18cab60b100666df59f2012845503c5
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-09-29 09:51:51 +0100

    Fix run.sh

--- run.sh
+++ run.sh
@@ -3,29 +3,43 @@
 g++ -o build/parser src/parser.cpp
 
 if [ $? -eq 0 ]; then
-  echo "parser.cpp compiled successfully"
+  echo -e "parser.cpp compiled successfully\n"
 else
-  echo "Error: Compilation of parser.cpp failed"
+  echo -e "Error: Compilation of parser.cpp failed\n"
 fi
 
 g++ -o build/vm src/vm.cpp
 
 if [ $? -eq 0 ]; then
-  echo "vm.cpp compiled successfully"
+  echo -e "vm.cpp compiled successfully\n"
 else
-  echo "Error: Compilation of vm.cpp failed"
+  echo -e "Error: Compilation of vm.cpp failed\n"
 fi
 
 input_dir="testcases/parserInputs"
 output_dir="testcases/parserOutputs"
 expected_output_dir="testcases/parserExpectedOutputs"
+tests=0
+successful_tests=0
 
 for input_file in "$input_dir"/*; do
   if [ -f "$input_file" ]; then
-    echo "Running test $input_file"
+    ((tests++))
+    echo -e "Running test $input_file"
     output_file="$output_dir/$(basename "$input_file").pout"
-    build/parser < "$input_file" > "$output_file"
+    touch "$output_file"
+    build/parser "$input_file" > "$output_file"
+    expected_output_file="$expected_output_dir/$(basename "$input_file").pout"
+    exit_code=$?
+    if [ $exit_code -eq 0 ] && diff -w -q "$output_file" "$expected_output_file" >/dev/null; then
+      echo -e "Test $input_file successful.\n"
+      ((successful_tests++))
+    else
+      echo -e "Test $input_file failed.\n"
+    fi
   fi
 done
+
+echo "$successful_tests out of $tests tests passed."
 
 read -p "Press Enter to exit..."
