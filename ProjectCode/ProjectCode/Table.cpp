cefb4817844b4616487993ab7f57abd7de92e4d5
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-09-29 07:57:03 +0100

    Improve project structure

--- ProjectCode/ProjectCode/Table.cpp
+++
@@ -1,9 +0,0 @@
-#include "Table.h"
-#include <vector>
-#include "SymbolTable.h"
-
-TableEntry::TableEntry(double location, double length) {
-	this->location = location;
-	this->length = length;
-
-}
--- ProjectCode/ProjectCode/parser.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-
-#include "parser.h"
-
